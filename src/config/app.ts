800aae8a5172eab51f32c94707f00abf22bd894d
unknown <416212316@qq.com>
2022-01-25 20:08:05 +0800

    fix

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,7 +2,8 @@ export const APP_NAME = '开卓教育'
 
 export const IMAGE_URL = 'https://mini-1304817606.file.myqcloud.com' // 静态资源的cos地址
 
-export const HTTP_REQUEST_URL = process.uniEnv.HTTP_REQUEST_URL
+// export const HTTP_REQUEST_URL = process.uniEnv.HTTP_REQUEST_URL
+export const HTTP_REQUEST_URL = 'https://mall.fsbaide.cn/'
 
 export const HEADER = {
   'content-type': 'application/json',
