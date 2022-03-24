7a2843f9060a9de4f498fd26eb007e9fc93fdc54
unknown <416212316@qq.com>
2022-03-24 22:34:33 +0800

    fix

--- src/pages/my/favorite.vue
+++ src/pages/my/favorite.vue
@@ -29,6 +29,74 @@
           </view>
         </scroll-view>
       </view>
+
+      <view class="set-wrap" catchtouchmove="true">
+        <view class="f-status dir-left-nowrap cross-center" :style="{ transform: 'translateY(' + statusTop + 'rpx)' }">
+          <view
+            class="f-item dir-left-nowrap cross-center main-between"
+            :style="{ color: rotate.left === 180 ? theme.color : '' }"
+            :class="rotate.left === 180 ? 'f-set' : 'f-def-set'"
+            @click="setDef('left')"
+          >
+            <view>
+              <text class="t-omit">{{ leftSet | category }}</text>
+            </view>
+            <view
+              class="bd-image"
+              :style="{
+                transform: 'rotate(' + rotate.left + 'deg)',
+                'background-color': rotate.left === 180 ? theme.background : '#353535',
+              }"
+            ></view>
+          </view>
+          <view
+            class="f-item dir-left-nowrap cross-center main-between"
+            :style="{ color: rotate.right === 180 ? theme.color : '' }"
+            :class="rotate.right === 180 ? 'f-set' : 'f-def-set'"
+            @click="setDef('right')"
+          >
+            <view>
+              <text class="t-omit">{{ rightSet | setStatus }}</text>
+            </view>
+            <view
+              class="bd-image"
+              :style="{
+                transform: 'rotate(' + rotate.right + 'deg)',
+                'background-color': rotate.right === 180 ? theme.background : '#353535',
+              }"
+            ></view>
+          </view>
+          <view class="f-good-btn" @click="edit">
+            {{ touch ? '完成' : '管理' }}
+          </view>
+          <view class="f-good-icon" @click="setListStyle()">
+            <image
+              :src="listStyle ? '/static/image/icon/favorite/square.png' : '/static/image/icon/favorite/row.png'"
+            ></image>
+          </view>
+        </view>
+        <view class="f-select" :style="{ transform: 'translateY(' + typeY + 'rpx)' }">
+          <scroll-view scroll-y>
+            <view class="dir-left-wrap f-type">
+              <view
+                v-for="it in statusList"
+                :key="it.id"
+                class="f-type-item"
+                :style="{ color: selectStatus == it.id ? theme.color : '' }"
+                :class="selectStatus == it.id ? '' : 'f-type-def'"
+                @tap="setStatus(it.id)"
+                >{{ it.name }}</view
+              >
+            </view>
+          </scroll-view>
+          <view class="f-button dir-left-nowrap">
+            <button class="box-grow-1" @click="sureStatus(0)">重置</button>
+            <button class="box-grow-1" :style="{ 'background-color': theme.background }" @click="sureStatus(1)">
+              确定
+            </button>
+          </view>
+        </view>
+      </view>
     </view>
   </view>
 </template>
@@ -51,11 +119,11 @@ export default defineComponent({
       rightSet: 0,
       statusTop: 85,
       listStyle: true,
-      statusList: [],
+      statusList: [] as any[],
       selectStatus: 0,
       typeY: -800,
       show: false,
-      list: [],
+      list: [] as any[],
       topicList: [],
       timeOutEvent: -1,
       touch: false,
@@ -119,12 +187,66 @@ export default defineComponent({
       }
     }
 
+    const setDef = (key) => {
+      if (key === 'right') {
+        state.statusList = [
+          {
+            name: '全部状态',
+            id: 0,
+          },
+          {
+            name: '优惠',
+            id: 3,
+          },
+          {
+            name: '低库存',
+            id: 1,
+          },
+          {
+            name: '失效',
+            id: 2,
+          },
+        ]
+        state.rotate.left = 0
+        state.typeY = -800
+      } else {
+        state.statusList = catList
+        state.rotate.right = 0
+        state.typeY = -800
+      }
+      if (state.rotate[key] === 0) {
+        state.rotate[key] = 180
+        state.show = true
+        state.typeY = -3
+      } else {
+        state.rotate[key] = 0
+      }
+    }
+
+    const setListStyle = () => {
+      state.listStyle = !state.listStyle
+      state.touch = false
+      if (!state.listStyle) {
+        for (let i = 0; i < state.list.length; i++) {
+          state.list[i].touch = false
+        }
+        state.allTouch = false
+      }
+    }
+
+    const setStatus = (index) => {
+      state.selectStatus = index
+    }
+
     return {
       ...toRefs(state),
       tabs,
       theme,
       closeMask,
       emitHandle,
+      setDef,
+      setListStyle,
+      setStatus,
     }
   },
 })
@@ -183,6 +305,122 @@ export default defineComponent({
             height: 4upx;
             left: 160upx;
             transition: 0.5s;
+          }
+        }
+      }
+    }
+    .set-wrap {
+      position: relative;
+      .f-status {
+        height: 78upx;
+        background-color: #ffffff;
+        border-bottom: 1upx solid #e2e2e2;
+        padding: 0 24upx;
+        transition: 0.5s;
+        z-index: 9;
+        position: absolute;
+        width: 100%;
+        top: 2upx;
+        > view:first-child {
+          margin-right: 22upx;
+        }
+        .f-item {
+          border-radius: 26upx;
+          height: 52upx;
+          margin-right: 5upx;
+          width: 160upx;
+          image {
+            width: 11upx;
+            height: 6upx;
+            transition: 0.3s;
+            margin-right: 16upx;
+          }
+          > view {
+            width: 133upx;
+            padding-left: 25upx;
+            padding-right: 10upx;
+          }
+          text {
+            width: 98upx;
+            font-size: 23upx;
+          }
+        }
+        .f-set {
+          border: 1upx solid;
+          background-color: #ffffff;
+        }
+        .f-def-set {
+          border: 1upx solid #f5f5f5;
+          background-color: #f5f5f5;
+        }
+
+        .f-img {
+          background-color: #353535;
+        }
+        text {
+          font-size: 23upx;
+        }
+        .f-good-icon {
+          width: 78upx;
+          height: 78upx;
+          position: absolute;
+          right: 0;
+          padding: 24upx;
+          image {
+            width: 30upx;
+            height: 30upx;
+          }
+        }
+        .f-good-btn {
+          position: absolute;
+          right: 79upx;
+          padding: 24upx;
+          font-size: 24upx;
+        }
+      }
+      .f-select {
+        position: absolute;
+        top: 0;
+        width: 750upx;
+        transition: 0.5s;
+        z-index: 8;
+        background-color: #f5f5f5;
+        .f-button {
+          height: 90upx;
+          border-top: 1upx solid #e2e2e2;
+          button {
+            font-size: 26upx;
+            line-height: 90upx;
+            border-radius: 0;
+          }
+          button:first-child {
+            color: #747474;
+            background-color: #fff;
+          }
+          button:last-child {
+            color: #fff;
+          }
+        }
+        .f-type {
+          padding: 48upx 24upx 24upx 24upx;
+          max-height: 360upx;
+          .f-type-item {
+            margin-bottom: 24upx;
+            background-color: #ffffff;
+            border: 1upx solid;
+            width: 339upx;
+            border-radius: 37upx;
+            height: 72upx;
+            line-height: 72upx;
+            font-size: 25upx;
+            text-align: center;
+          }
+          .f-type-def {
+            border: 1upx solid #e2e2e2;
+            color: #353535;
+          }
+          .f-type-item:nth-child(odd) {
+            margin-right: 23upx;
           }
         }
       }
