ed40ec939d9d88cd1f8b7b4b6a2056a02d204e46
unknown <416212316@qq.com>
2022-03-26 21:05:43 +0800

    detail

--- src/pages/answering-question/detail.vue
+++ src/pages/answering-question/detail.vue
@@ -1,7 +1,19 @@
 <template>
   <view class="container">
-    <view>{{ detail.title }}</view>
-    <view><rich-text :nodes="detail.content"></rich-text></view>
+    <view class="header">
+      <view class="title">{{ detail.title }}</view>
+      <view class="title-bar">
+        <view class="title-bar-left"><text class="iconfont icon-calendar"></text>{{ detail.created_at }}</view>
+        <view class="title-bar-right">
+          <view class="favorite"> <text class="iconfont icon-love"></text> 收藏 </view>
+          <view class="share"> <text class="iconfont icon-share"></text> 分享 </view>
+        </view>
+      </view>
+    </view>
+    <view class="detail">
+      <image src="/static/icon/goods-detail.png"></image>
+      <rich-text :nodes="detail.content"></rich-text>
+    </view>
   </view>
 </template>
 <script setup lang="ts">
@@ -31,6 +43,73 @@ onLoad((options) => {
 })
 </script>
 <style lang="scss">
+@import '@/static/css/variable.scss';
 .container {
+  padding: 20rpx;
+  .header {
+    background-color: #fff;
+    border-radius: 15rpx;
+    .title {
+      font-size: 42rpx;
+      font-weight: 500;
+      text-align: justify;
+      color: #353535;
+      padding: 20rpx;
+    }
+    .title-bar {
+      padding-top: 20rpx;
+      padding-bottom: 20rpx;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 28rpx;
+      color: #999;
+      .title-bar-left {
+        padding-left: 20rpx;
+        .iconfont {
+          padding-right: 10rpx;
+          color: $theme-font-color;
+        }
+      }
+      .title-bar-right {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        .iconfont {
+          color: $theme-font-color;
+        }
+        .favorite {
+          padding-right: 20rpx;
+        }
+        .share {
+          color: #fff;
+          height: 48rpx;
+          line-height: 48rpx;
+          font-size: 22rpx;
+          border-radius: 40rpx 0 0 40rpx;
+          padding: 0 14rpx;
+          width: 103rpx;
+          background-color: $theme-font-color;
+
+          .iconfont {
+            color: #fff;
+            font-size: 24rpx;
+          }
+        }
+      }
+    }
+  }
+  .detail {
+    width: 702upx;
+    padding: 20upx 0;
+    background-color: #ffffff;
+    border-radius: 15upx;
+    margin-top: 24upx;
+    image {
+      width: 100%;
+      height: 80rpx;
+      display: block;
+    }
+  }
 }
 </style>
